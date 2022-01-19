ac1a277629f626e0699d4632f59e4a4aab0e80ae
Gimenez Botella <ignacio.gimenez@getronics.com>
2022-01-19 13:30:01 +0100

    Change type

--- src/com/hiberus/challenge/Employee.java
+++ src/com/hiberus/challenge/Employee.java
@@ -5,7 +5,7 @@ public class Employee {
 	String name;
 	Integer age;
 
-	public Employee(String name, int age) {
+	public Employee(String name, Integer age) {
 		this.name = name;
 		this.age = age;
 	}
